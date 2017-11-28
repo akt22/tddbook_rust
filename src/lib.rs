402dc01aba4ba3575db49862c39b7d2b3746f831
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-11-29 00:36:11 +0900

    chp 1: test passed in Rust!!

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
+#[derive(Debug)]
+pub struct Dollar {
+    amount: i32,
+}
+
+impl Dollar {
+    fn new(amount: i32) -> Dollar {
+        Dollar { amount: amount }
+    }
+    fn times(&mut self, multiplier: i32) {
+        self.amount *= multiplier;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn test_multiplication() {
+        let mut five = Dollar::new(5);
+        five.times(2);
+        assert_eq!(10, five.amount);
     }
 }
