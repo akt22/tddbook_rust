de77af80123295d39adec6516dc96ba9fbc489b3
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-11-30 00:07:11 +0900

    chp3: finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Dollar {
     amount: i32,
 }
 
 impl Dollar {
-    fn new(amount: i32) -> Dollar {
+    pub fn new(amount: i32) -> Dollar {
         Dollar { amount: amount }
     }
-    fn times(&self, multiplier: i32) -> Dollar {
+    pub fn times(&self, multiplier: i32) -> Dollar {
         Dollar { amount: self.amount * multiplier }
+    }
+
+    pub fn equals(&self, object: Dollar) -> bool {
+        let dollar = object;
+        self.amount == dollar.amount
     }
 }
 
@@ -23,5 +28,11 @@ mod tests {
         assert_eq!(10, product.amount);
         let product = five.times(3);
         assert_eq!(15, product.amount);
+    }
+
+    #[test]
+    fn test_equality() {
+        assert!(Dollar::new(5).equals(Dollar::new(5)));
+        assert_eq!(false, Dollar::new(5).equals(Dollar::new(6)));
     }
 }
