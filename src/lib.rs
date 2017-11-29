18106a28ed382e76299fadfc2a84cfbbcaa5498e
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-11-30 00:08:50 +0900

    chp4: finished

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ impl Dollar {
     pub fn new(amount: i32) -> Dollar {
         Dollar { amount: amount }
     }
+
     pub fn times(&self, multiplier: i32) -> Dollar {
         Dollar { amount: self.amount * multiplier }
     }
@@ -24,10 +25,8 @@ mod tests {
     #[test]
     fn test_multiplication() {
         let five = Dollar::new(5);
-        let product = five.times(2);
-        assert_eq!(10, product.amount);
-        let product = five.times(3);
-        assert_eq!(15, product.amount);
+        assert_eq!(Dollar::new(10), five.times(2));
+        assert_eq!(Dollar::new(15), five.times(3));
     }
 
     #[test]
